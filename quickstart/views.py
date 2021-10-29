133a0b14c6cba2205b0bd08a0c5b77c46d323faf
saikiran reddy <saikiran199@gmail.com>
2021-10-29 12:51:34 +0530

    initial restframework

---
+++ quickstart/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+# Create your views here.
+
+@api_view()
+def home(request):
+    return Response({'status':200,"message":'this is 1st rest api pro'})
